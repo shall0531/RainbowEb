24b497b4110d9a3f7a7409b5f34acd7592547cf6
Esiot <shall0531@hotmail.com>
2018-07-12 22:09:27 +0200

    feat(login): gestion de page login de rainbow

---
+++ src/app/components/login/login.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { Validators, FormBuilder, FormGroup, Form } from '@angular/forms';
+import { CoreService } from '../../services/core.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+  hide = true;
+  form: FormGroup;
+
+  constructor(private fb: FormBuilder,
+    private coreService: CoreService) {
+    this.form = this.fb.group({
+      login: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+  }
+
+  run() {
+    this.coreService.runRainbow(this.form.value['login'], 
+                                this.form.value['password']);
+    setTimeout (() => {
+                                  console.log(this.coreService.account);
+                               }, 2000);
+  }
+  ngOnInit() {
+
+  }
+
+}

499bd9f394b28a1c93de2ba2139f086cfbe14216
Esiot <shall0531@hotmail.com>
2018-07-16 22:01:43 +0200

    feat(contact): initial parti contact, front

---
+++ src/app/components/contact/contact.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { CoreService } from '../../services/core.service';
+import { ContactModel } from '../../models/contact.model';
+
+@Component({
+  selector: 'app-contact',
+  templateUrl: './contact.component.html',
+  styleUrls: ['./contact.component.less']
+})
+export class ContactComponent implements OnInit {
+  @Input('object') contact: ContactModel;
+  constructor(private coreService: CoreService) { 
+  }
+
+  ngOnInit() {
+  }
+
+}
